4b18edee781a82da858b150a987b6ae59c9ac3b1
mr.Shu <mr@shu.io>
2016-02-04 23:53:01 +0100

    update: Add diaqres structure and clean_dump
    * Add diaqres directory structure and first util function: clean_dump
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ diaqres/utils.py
@@ -0,0 +1,18 @@
+import re
+import sys
+TAGS = re.compile(r'<[^>]+>', re.IGNORECASE)
+DEBREE = re.compile(r'\%.+>', re.IGNORECASE)
+TABLE_DEBREE = re.compile(r'^\![^\|]+\|', re.IGNORECASE)
+PX_DEBREE = re.compile(r'px[^>]+>', re.IGNORECASE)
+
+
+def clean_dump(filename):
+    outfile = '{}.clean'.format(filename)
+    with open(filename, 'r') as f:
+        with open(outfile, 'w') as out:
+            for line in f:
+                line = TAGS.sub('', line)
+                line = DEBREE.sub('', line)
+                line = TABLE_DEBREE.sub('', line)
+                line = PX_DEBREE.sub('', line)
+                out.write(line)
