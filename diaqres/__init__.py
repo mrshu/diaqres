9fe3ec5b3375064baf26b918c6c5e5797e2e6121
mr.Shu <mr@shu.io>
2016-02-06 15:45:19 +0100

    update: Add setup.py, improve CLI interface
    * Add the first version of setup.py to simplify instalation.
    
    * Improve CLI interface to include clean-dump command.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- diaqres/__init__.py
+++ diaqres/__init__.py
@@ -1,7 +1,21 @@
+import click
 
 
+@click.group()
 def main():
     pass
+
+
+@main.command('clean-dump')
+@click.option('--filename',
+              type=click.Path(exists=True),
+              required=True)
+def clean_dump(filename):
+    from .utils import clean_dump as clean
+    click.echo('Starting to cleaning {}'.format(filename))
+    clean(filename)
+    click.echo('Finished cleaning {}'.format(filename))
+
 
 if __name__ == '__main__':
     main()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='diaqres',
+
+    description='Diacritics Restoration toolkit',
+    author='Marek Suppa',
+    author_email='marek@suppa.sk',
+    version='0.0.1',
+
+    url='https://github.com/mrshu/diaqres',
+
+    include_package_data=True,
+    install_requires=['Click'],
+    packages=['diaqres'],
+
+    license="GPL 3.0",
+    keywords=['nlp', 'diacritics restoration'],
+    entry_points={
+        'console_scripts': [
+            'diaqres = diaqres:main'
+        ]
+    }
+)
